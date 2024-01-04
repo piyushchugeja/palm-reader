a8af996c91175cca8aa5a3f79fe12f476b9a4241
Piyush Chugeja <piyushchugeja@gmail.com>
2024-01-04 23:42:53 +0530

    main file

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import google.generativeai as genai
+import PIL.Image
+import os
+from re import sub
+
+genai.configure(api_key=st.secrets['GOOGLE_API_KEY'])
+model = genai.GenerativeModel('gemini-pro-vision')
+
+st.header("Palm Reader 🔮")
+st.subheader("A funny initiative to bring palm reading to the 21st century. Ideated by @anushreekhadye, developed by @piyushchugeja, & powered by Google's Gemini AI.")
+
+question = st.text_input("Ask a question about your life and the AI will answer it based on the readings of your palm.")
+submit = st.button("Submit")
+
+if 'palm_image' not in st.session_state:
+    st.session_state.palm_image = None
+
+
+with st.sidebar:
+    st.subheader("Upload image")
+    file_raw = st.file_uploader("Upload an image of your palm here and click on 'Process'")
+    if st.button("Process"):
+        with st.spinner("Processing..."):
+            try:
+                st.session_state.palm_image = PIL.Image.open(file_raw)
+                st.success("Done!")
+            except:
+                st.error("Please upload an image of your palm.")
+                st.session_state.palm_image = None
+
+if submit:
+    with st.spinner("Processing..."):
+        try:
+            if question and st.session_state.palm_image:
+                prompt = "Read the palm of this person and answer the following question: " + question + ". If there is no palm in the image, answer 'I cannot read the palm of this person.'"
+                response = model.generate_content(
+                    contents = [question, st.session_state.palm_image]
+                )
+                st.success(sub(r'(?<=\w)([A-Z])', r' \1', response.text))
+            elif not question:
+                st.error("Please ask a question.")
+            elif not st.session_state.palm_image:
+                st.error("Please upload an image of your palm.")
+        except Exception as e:
+            print(e)
+            st.error("Something went wrong. Please try again.")
